0a8e89565e3e4109e2a46b6844b28955047f5ebf
Administrator <623240480@qq.com>
2016-07-30 09:39:42 +0800

    sql create file

--- db/DatabaseQRCode_20160730.sql
+++ db/DatabaseQRCode_20160730.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `qrcode` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `qrcode`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: qrcode
@@ -29,7 +31,7 @@ CREATE TABLE `qrtable` (
   `objectid` int(11) DEFAULT NULL,
   PRIMARY KEY (`qrid`),
   UNIQUE KEY `objectid_UNIQUE` (`objectid`)
-) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +40,7 @@ CREATE TABLE `qrtable` (
 
 LOCK TABLES `qrtable` WRITE;
 /*!40000 ALTER TABLE `qrtable` DISABLE KEYS */;
-INSERT INTO `qrtable` VALUES (76,'1469688223','		<A><h>0F02333</h><p><n>3213213</n> <r>13Y000送风机状态</r><r>24Y001抽风机状态</r><r>35Y002冷却风机状态</r><r>46Y003冷却风泵状态</r><r>57Y004循环水泵状态</r> </p><p><n>5实时数据</n> <r>11D212顺序启动延时</r><r>22D1FE变频器频率</r><r>31D212顺序启动延时</r><r>42D1FE变频器频率</r><r>51D212顺序启动延</r></p></A>\n		',1223),(77,'1469689714','<A><h>0F02333</h><p><n>3213213</n> <r>13Y000送风机状态</r><r>24Y001抽风机状态</r><r>35Y002冷却风机状态</r><r>46Y003冷却风泵状态</r><r>57Y004循环水泵状态</r> </p><p><n>5实时数据</n> <r>11D212顺序启动延时</r><r>22D1FE变频器频率</r><r>31D212顺序启动延时</r><r>42D1FE变频器频率</r><r>51D212顺序启动延</r></p></A>',3123);
+INSERT INTO `qrtable` VALUES (76,'1469688223','{json:data}',1223),(77,'1469689714','<A><h>0F02333</h><p><n>3213213</n> <r>13Y000送风机状态</r><r>24Y001抽风机状态</r><r>35Y002冷却风机状态</r><r>46Y003冷却风泵状态</r><r>57Y004循环水泵状态</r> </p><p><n>5实时数据</n> <r>11D212顺序启动延时</r><r>22D1FE变频器频率</r><r>31D212顺序启动延时</r><r>42D1FE变频器频率</r><r>51D212顺序启动延</r></p></A>',3123),(78,'1469841146','		<A><h>0F02333</h><p><n>3213213</n> <r>13Y000送风机状态</r><r>24Y001抽风机状态</r><r>35Y002冷却风机状态</r><r>46Y003冷却风泵状态</r><r>57Y004循环水泵状态</r> </p><p><n>5实时数据</n> <r>11D212顺序启动延时</r><r>22D1FE变频器频率</r><r>31D212顺序启动延时</r><r>42D1FE变频器频率</r><r>51D212顺序启动延</r></p></A>\n		',1224);
 /*!40000 ALTER TABLE `qrtable` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-29  9:39:57
+-- Dump completed on 2016-07-30  9:25:27
